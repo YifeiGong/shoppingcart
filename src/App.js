4516399793efd168663315ef60c52c84d6e98c01
YifeiGong <yifeiaimeegong@hotmail.com>
2019-09-29 13:20:05 -0500

    Get and test your data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase/app';
+import 'firebase/database';
 
-function App() {
+const firebaseConfig = {
+  apiKey: "AIzaSyDgxBkbLBHoutLJZcT6G0iDEoRcp_YXxK0",
+  authDomain: "shoppingcart-fac8e.firebaseapp.com",
+  databaseURL: "https://shoppingcart-fac8e.firebaseio.com",
+  projectId: "shoppingcart-fac8e",
+  storageBucket: "",
+  messagingSenderId: "479899425927",
+  appId: "1:479899425927:web:d2bd65e9ba6aa2911a6c22"
+};
+
+
+const App = () => {
+  const [data, setData] = useState({});
+  const products = Object.values(data);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch('./data/products.json');
+      const json = await response.json();
+      setData(json);
+    };
+    fetchProducts();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      {products.map(product => <li key={product.sku}>{product.title}</li>)}
+    </ul>
   );
-}
+};
 
 export default App;
